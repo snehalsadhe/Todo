7189a29dfec4f2bc3e10e301949e8747e0568db0
snehaladhe <snehaladhe4768@gmail.com>
2024-11-11 17:45:06 +0530

    adding changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Todo from "./Todo"
 function App() {
   return (
     <>  
-     <h1 style={{ textAlign: 'center', color: '#343a40', marginTop:'100px' }}>Todo List</h1>
+     <h1 style={{ textAlign: 'center', color: '#343a40', marginTop:'100px' }}>My Todo List</h1>
       <Todo/>
     </>
   )
