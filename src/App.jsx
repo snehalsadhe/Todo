42f484a14441d0c2b2fc07cf5040a402a269008f
snehalsadhe <snehalsadhe@gmail.com>
2024-12-30 21:10:44 +0530

    using context seperated business logic and Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,80 @@
 import AppName from "./components/AppName";
 import Todo from "./components/Todo"
-import { useState, useReducer } from "react";
+
 import TodoMap from "./components/TodoMap";
-import { ItemsContext } from "./contextStore/todoItemsContext";
+import  ItemsContextProvider  from "./contextStore/todoItemsContext";
 
-const itemReducer=(currentItem,action)=>{
+// const itemReducer=(currentItem,action)=>{
 
- let newTodoItems=currentItem
-  if(action.type=="NEW_ITEM"){
+//  let newTodoItems=currentItem
+//   if(action.type=="NEW_ITEM"){
 
-    newTodoItems=[
-      ...currentItem,
-      {
-        itemId:Date.now(),
-        item:action.payload.itemName
-      }
-    ]
-  }
-  else if(action.type=="DELETE_ITEM"){
-    newTodoItems=currentItem.filter((i)=>i.item!==action.payload.item)
-  }
-  return newTodoItems
+//     newTodoItems=[
+//       ...currentItem,
+//       {
+//         itemId:Date.now(),
+//         item:action.payload.itemName
+//       }
+//     ]
+//   }
+//   else if(action.type=="DELETE_ITEM"){
+//     newTodoItems=currentItem.filter((i)=>i.item!==action.payload.item)
+//   }
+//   return newTodoItems
 
-}
+// }
 
 function App() {
 //const [itemList,setItemList]=useState([]);
-const [itemList,dispatcherItem]=useReducer(itemReducer,[]);
+// const [itemList,dispatcherItem]=useReducer(itemReducer,[]);
 
-const addButton=(itemName)=>{
+// const addButton=(itemName)=>{
 
-  const newItemAction={
-    type:"NEW_ITEM",
-    payload:{
-      itemName
-    }
-  }
+//   const newItemAction={
+//     type:"NEW_ITEM",
+//     payload:{
+//       itemName
+//     }
+//   }
 
-  dispatcherItem(newItemAction)
+//   dispatcherItem(newItemAction)
 
 
 
-  // setItemList((currentValue)=>[
-  //   ...currentValue, //react refer itemList here as its part of react to inject the things for you here setItemList is used.
-  //   {
-  //     itemId:Date.now(),
-  //     item:itemName
-  //   }
-  // ])
+//   // setItemList((currentValue)=>[
+//   //   ...currentValue, //react refer itemList here as its part of react to inject the things for you here setItemList is used.
+//   //   {
+//   //     itemId:Date.now(),
+//   //     item:itemName
+//   //   }
+//   // ])
 
 
   
-}
-const deleteButton=(itemName)=>{
- const deleteItemAction={
-  type:"DELETE_ITEM",
-  payload:{
-     item:itemName
-  }
- }
+// }
+// const deleteButton=(itemName)=>{
+//  const deleteItemAction={
+//   type:"DELETE_ITEM",
+//   payload:{
+//      item:itemName
+//   }
+//  }
 
- dispatcherItem(deleteItemAction)
+//  dispatcherItem(deleteItemAction)
 
-//   const newItemList=itemList.filter((i)=>i.item!==itemName)
-// setItemList(newItemList)
-}
+// //   const newItemList=itemList.filter((i)=>i.item!==itemName)
+// // setItemList(newItemList)
+// }
 
 
   return (
     <>  
-    <ItemsContext.Provider value={{itemList,addButton,deleteButton}}>
+   
+    <ItemsContextProvider>
     <AppName/>
     <Todo />
     <TodoMap/>
-    </ItemsContext.Provider>
+    </ItemsContextProvider>
     </>
 
   )
--- src/contextStore/todoItemsContext.jsx
+++ src/contextStore/todoItemsContext.jsx
@@ -1,5 +1,59 @@
 import { createContext } from "react";
+import {useReducer} from "react"
+
 
 export const ItemsContext=createContext({itemList:[],
   addButton:()=>{},
   deleteButton:()=>{}});
+
+  const itemReducer=(currentItem,action)=>{
+
+    let newTodoItems=currentItem
+     if(action.type=="NEW_ITEM"){
+   
+       newTodoItems=[
+         ...currentItem,
+         {
+           itemId:Date.now(),
+           item:action.payload.itemName
+         }
+       ]
+     }
+     else if(action.type=="DELETE_ITEM"){
+       newTodoItems=currentItem.filter((i)=>i.item!==action.payload.item)
+     }
+     return newTodoItems
+   
+   }
+  const ItemsContextProvider=({children})=>{
+
+    const [itemList,dispatcherItem]=useReducer(itemReducer,[]);
+
+const addButton=(itemName)=>{
+
+  const newItemAction={
+    type:"NEW_ITEM",
+    payload:{
+      itemName
+    }
+  }
+  dispatcherItem(newItemAction)
+}
+const deleteButton=(itemName)=>{
+ const deleteItemAction={
+  type:"DELETE_ITEM",
+  payload:{
+     item:itemName
+  }
+ }
+
+ dispatcherItem(deleteItemAction)
+}
+
+return(
+<ItemsContext.Provider value={{itemList,addButton,deleteButton}}>{children}</ItemsContext.Provider>
+);
+  }
+
+  export default ItemsContextProvider;
+  
